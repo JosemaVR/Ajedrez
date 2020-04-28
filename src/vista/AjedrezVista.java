f60449c9eb3362d8a97b6275a556fd53d82078fb
Francisco José <fran.kisko98@gmail.com>
2020-04-28 16:27:16 +0200

    1.0

---
+++ src/vista/AjedrezVista.java
@@ -0,0 +1,63 @@
+package vista;
+
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import javax.swing.JFrame;
+
+public class AjedrezVista extends JFrame implements WindowListener {
+	
+	private static final long serialVersionUID = 1L;
+
+	public AjedrezVista() {
+		
+		this.setSize(500,500);
+		this.setTitle("Chess");
+		this.setVisible(true);
+		this.addWindowListener(this);
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		// TODO Auto-generated method stub
+		System.exit(0);
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
