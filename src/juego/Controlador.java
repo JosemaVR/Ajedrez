4ce8e09c7371d0ae299da524f8b3d1a463903b93
JosemaVR <josevillalon93@gmail.com>
2020-05-06 12:48:34 +0200

    "Avances" en controlador

--- src/juego/Controlador.java
+++ src/juego/Controlador.java
@@ -1,78 +1,81 @@
 package juego;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
-import versionCero.Tablero_v3;
+import javax.swing.BorderFactory;
+import javax.swing.border.Border;
 
 public class Controlador implements WindowListener, ActionListener
 {
 	Vista objVista = null;
 	Modelo objModelo = null;
-	Tablero_v3 pieza = null;
+	Border bordeRojo = BorderFactory.createLineBorder(Color.red);
+	Integer i, j;
 
-	public Controlador(Vista objVista, Modelo objModelo)
+	public Controlador(Modelo objModelo, Vista objVista)
 	{
-
+		for(i=0; i<8; i++) 
+		{
+			for(j=0; j<8; j++) {
+				objVista.casillas[i][j].addActionListener(this);
+			}
+		}
+		objVista.addWindowListener(this);
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent arg0)
-	{
+	public void actionPerformed(ActionEvent arg0) {
 		Object fuente = arg0.getSource();
-
-		objModelo.botonAccion(fuente, 7, 1, objVista.casillas, pieza.caballoBa2 );
-
+		if (objVista.casillas[0][0].equals(fuente))
+		{
+			objVista.casillas[0][0].setBorder(bordeRojo);
+			System.out.println("Funciona en " + i + " " + j);
+		}			
 	}
 
 	@Override
 	public void windowActivated(WindowEvent arg0)
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void windowClosed(WindowEvent arg0)
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void windowClosing(WindowEvent arg0)
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void windowDeactivated(WindowEvent arg0)
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void windowDeiconified(WindowEvent arg0)
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void windowIconified(WindowEvent arg0)
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void windowOpened(WindowEvent arg0)
 	{
 		// TODO Auto-generated method stub
-
 	}
 }
--- src/juego/Principal.java
+++ src/juego/Principal.java
@@ -6,8 +6,8 @@ public class Principal {
 
 	public static void main(String[] args) {
 		Vista objVista = new Vista();
-		//Modelo objModelo = new Modelo();
-		//new Controlador(objModelo, objVista);
+		Modelo objModelo = new Modelo();
+		new Controlador(objModelo, objVista);
 		System.out.println("[" + LocalTime.now() + "][Inicio del programa]");
 	}
 }
