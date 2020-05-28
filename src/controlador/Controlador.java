8baf00aa22c0cbae029a80080e648c394e7c5b7c
JosemaVR <josevillalon93@gmail.com>
2020-05-28 21:31:58 +0200

    Arreglos en Pieza y movimientos

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -78,6 +78,7 @@ public class Controlador implements WindowListener, ActionListener
 		//TODO Ocultar todos los frames salvo el pulsado
 		
 		if (fuente.equals(objVista.getMniPartidaRapida())) {
+			//objVista.setVisible(false);
 			objVista.getDlgNuevaPartida().setVisible(true);
 			objVista.getFrmAyuda().setVisible(false);
 			objVista.getFrmClasificacionJugador().setVisible(false);
