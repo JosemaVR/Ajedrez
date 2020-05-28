2e955cb8a51d0e9cde4b9cab57c2e1d94f060be9
David <david.cabello.info@gmail.com>
2020-05-28 11:39:33 +0200

    creacion interfaz Vista

--- src/juego/Vista.java
+++ src/juego/Vista.java
@@ -3,20 +3,72 @@ package juego;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
+
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.border.Border;
-import javax.swing.JButton;
-import javax.swing.JFrame;
 
-public class Vista extends JFrame {
+public class Vista extends JFrame 
+{
 
 	private static final long serialVersionUID = 1L;
+
+	JFrame frmInicio = new JFrame();
+	JMenuBar barraMenu = new JMenuBar();
+	JMenu mnJugar = new JMenu();
+	JMenu mnClasificacion = new JMenu();
+	JMenu mnAyuda = new JMenu();
+
+	JMenuItem mniPartidaRapida = new JMenuItem("Partida Rápida");
+	JMenuItem mniSalir = new JMenuItem("Salir");
+	JMenuItem mniJugador = new JMenuItem("Jugador");
+	JMenuItem mniPartidas = new JMenuItem("Partidas");
+	JMenuItem mniAyuda = new JMenuItem("Ayuda");
+
+	// Etiquetas Menú Principal
+
+	JLabel lblNombreJ1 = new JLabel("Nombre Jugador 1");
+	JLabel lblNombreJ2 = new JLabel("Nombre Jugador 2");
+	JLabel lblTipoPartida = new JLabel("Tipo de Partida");
+	JLabel lblSeleccionColor = new JLabel("Seleccionar color");
+	JLabel lblLimiTiempo = new JLabel("Límite de tiempo");
+	JLabel lblNombre = new JLabel("Nombre");
+
+	// Botones Menú Principal
+
+	JButton btnLanzar = new JButton("Lanzar");
+	JButton btnEmpezar = new JButton("Empezar");
+	JButton btnCancelar = new JButton("Cancelar");
+	JButton btnMostrar = new JButton("Mostrar");
+	JButton btnVolver = new JButton("Volver");
+
+	// TextFields Menú Principal
+
+	JTextField txtNombre = new JTextField();
+	JTextField txtNombreJ1 = new JTextField();
+	JTextField txtNombreJ2 = new JTextField();
+	JTextField txtSeleccionColor = new JTextField();
+
+	// Choices Menú Principal
+	boolean selected = false;
+
+	JCheckBox chkMejorTres = new JCheckBox("¿Al mejor de tres?", selected);
+	JCheckBox chkUnoVsUno = new JCheckBox("Jugador contra Jugador", selected);
+	JCheckBox chkUnoVsIa = new JCheckBox();
+
 	// Panel contenedor
 	JPanel pantalla = new JPanel();
 	JPanel panelCentral = new JPanel();
@@ -27,7 +79,7 @@ public class Vista extends JFrame {
 
 	// Límites
 	Border borderContents = BorderFactory.createEmptyBorder(10, 10, 10, 10);
-	//private Border borderRegion = BorderFactory.createLineBorder(Color.RED, 1);
+	// private Border borderRegion = BorderFactory.createLineBorder(Color.RED, 1);
 
 	// Colores
 	Color colorContenedores = Color.WHITE; // Panel contenedor
@@ -35,7 +87,7 @@ public class Vista extends JFrame {
 	// Fuentes:
 	Font fuenteEtiquetas = new Font(Font.DIALOG, Font.BOLD, 16);
 
-	//Labels
+	// Labels
 	JLabel lblEste = new JLabel("Texto panel ESTE");
 	JLabel lblOeste = new JLabel("Texto panel OESTE");
 	JLabel lblNorte = new JLabel("Texto panel NORTE");
@@ -59,7 +111,31 @@ public class Vista extends JFrame {
 	Integer i, j;
 	ImageIcon caballoBa2 = new ImageIcon("./imagenes/sin_fondo/Caballo_blanco.png");
 
-	public Vista() {
+	public Vista()
+	{
+
+		frmInicio.setTitle("Menú Principal");
+		frmInicio.setJMenuBar(barraMenu);
+		
+		
+		mnJugar.add(mniPartidaRapida);
+		mnJugar.addSeparator();
+		mnJugar.add(mniSalir);
+		
+		mnClasificacion.add(mniJugador);
+		mnClasificacion.add(mniPartidas);
+		mnAyuda.add(mniAyuda);
+
+		
+		frmInicio.setSize(300, 300);
+		frmInicio.setLocationRelativeTo(null);
+		frmInicio.setVisible(true);
+		frmInicio.setResizable(false);
+
+		barraMenu.add(mnJugar);
+		barraMenu.add(mnClasificacion);	
+		barraMenu.add(mnAyuda);
+		
 		setTitle("Tablero");
 		UIManager.put("Label.font", fuenteEtiquetas);
 
@@ -67,40 +143,40 @@ public class Vista extends JFrame {
 		for (i = 0; i < 9; i++)
 		{
 			for (j = 0; j < 9; j++, letra++)
-			{	
-				if(i==0 && j==0) {
+			{
+				if (i == 0 && j == 0)
+				{
 					fichaElegida.setIcon(caballoBa2);
 					fichaElegida.setPreferredSize(new Dimension(70, 70));
 					tablero.add(fichaElegida);
-					//En un futuro añadir cambios aqui segun la pieza seleccionada o algo así
-				}
-				else if(i==0 || i==9)  {
-					letTablero[j] = new JLabel(Character.toString((char) (letra-1)));
+					// En un futuro añadir cambios aqui segun la pieza seleccionada o algo así
+				} else if (i == 0 || i == 9)
+				{
+					letTablero[j] = new JLabel(Character.toString((char) (letra - 1)));
 					letTablero[j].setHorizontalAlignment(JLabel.CENTER);
 					letTablero[j].setVerticalAlignment(JLabel.CENTER);
 					tablero.add(letTablero[j]);
-				}
-				else 
+				} else
 				{
-					if((j==0 && (i!=0 || i!=9))) {
+					if ((j == 0 && (i != 0 || i != 9)))
+					{
 						numTablero[i] = new JLabel(Integer.toString(i));
 						numTablero[i].setHorizontalAlignment(JLabel.CENTER);
 						numTablero[i].setVerticalAlignment(JLabel.CENTER);
 						tablero.add(numTablero[i]);
-					}
-					else 
+					} else
 					{
-						casillas[i-1][j-1] = new JButton((i-1) + ", " + (j-1));
-						casillas[i-1][j-1].setPreferredSize(new Dimension(70, 70));
+						casillas[i - 1][j - 1] = new JButton((i - 1) + ", " + (j - 1));
+						casillas[i - 1][j - 1].setPreferredSize(new Dimension(70, 70));
 						if ((i + j) % 2 != 0)
 						{
-							casillas[i-1][j-1].setBackground(colorNegro);
+							casillas[i - 1][j - 1].setBackground(colorNegro);
 						}
 						if ((i + j) % 2 == 0)
 						{
-							casillas[i-1][j-1].setBackground(colorBlanco);
+							casillas[i - 1][j - 1].setBackground(colorBlanco);
 						}
-						tablero.add(casillas[i-1][j-1]);
+						tablero.add(casillas[i - 1][j - 1]);
 					}
 				}
 			}
@@ -122,9 +198,9 @@ public class Vista extends JFrame {
 		panelSur.add(lblSur);
 		add(panelSur, BorderLayout.SOUTH);
 
-		setSize(1100,800);
+		setSize(1100, 800);
 		setResizable(false);
 		setLocationRelativeTo(null);
-		setVisible(true);
-	}	
+		setVisible(false);
+	}
 }
