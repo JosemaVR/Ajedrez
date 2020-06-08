7260c81e796fea28e8c8285b0c3ee63205cacac3
JosemaVR <josevillalon93@gmail.com>
2020-06-08 20:29:45 +0200

    Piezas movidas a tipo Pieza para simplificar movimientos

--- src/modelo/Modelo.java
+++ src/modelo/Modelo.java
@@ -51,51 +51,6 @@ public class Modelo
 		}
 		return con;
 	}
-	/*
-	public void nuevoJuego(JButton[][] casillas) {
-		for (int i = 0; i < 8; i++)
-		{
-			for (int j = 0; j < 8; j++)
-			{
-				getTablero()[i][j] = null;
-			}
-		}
-		getTablero()[0][0] = "Tn1";
-		getTablero()[0][1] = "Cn1";
-		getTablero()[0][2] = "An1";
-		getTablero()[0][3] = "Qn1";
-		getTablero()[0][4] = "Kn1";
-		getTablero()[0][5] = "An2";
-		getTablero()[0][6] = "Cn2";
-		getTablero()[0][7] = "Tn2";
-		getTablero()[1][0] = "Pn1";
-		getTablero()[1][1] = "Pn2";
-		getTablero()[1][2] = "Pn3";
-		getTablero()[1][3] = "Pn4";
-		getTablero()[1][4] = "Pn5";
-		getTablero()[1][5] = "Pn6";
-		getTablero()[1][6] = "Pn7";
-		getTablero()[1][7] = "Pn8";
-
-		getTablero()[7][0] = "Tb1";
-		getTablero()[7][1] = "Cb1";
-		getTablero()[7][2] = "Ab1";
-		getTablero()[7][3] = "Qb1";
-		getTablero()[7][4] = "Kb1";
-		getTablero()[7][5] = "Ab2";
-		getTablero()[7][6] = "Cb2";
-		getTablero()[7][7] = "Tb2";
-		getTablero()[6][0] = "Pb1";
-		getTablero()[6][1] = "Pb2";
-		getTablero()[6][2] = "Pb3";
-		getTablero()[6][3] = "Pb4";
-		getTablero()[6][4] = "Pb5";
-		getTablero()[6][5] = "Pb6";
-		getTablero()[6][6] = "Pb7";
-		getTablero()[6][7] = "Pb8";
-
-		setIcons(getTablero(), casillas);
-	}*/
 
 	public Random nuevoTurno() {
 		Random turno = new Random(1);
@@ -112,136 +67,6 @@ public class Modelo
 		}
 		return turno;
 	}
-
-	/*
-	public static void setIcons(String[][] tablero, JButton[][] casillas) {
-		ImageIcon pn = new ImageIcon("./imagenes/sin_fondo/Peon_negro.png");
-		ImageIcon pb = new ImageIcon("./imagenes/sin_fondo/Peon_blanco.png");
-
-		ImageIcon cn = new ImageIcon("./imagenes/sin_fondo/Caballo_negro.png");
-		ImageIcon tn = new ImageIcon("./imagenes/sin_fondo/Torre_negra.png");
-		ImageIcon an = new ImageIcon("./imagenes/sin_fondo/Alfil_negro.png");
-		ImageIcon qn = new ImageIcon("./imagenes/sin_fondo/Reina_negra.png");
-		ImageIcon kn = new ImageIcon("./imagenes/sin_fondo/Rey_negro.png");
-
-		ImageIcon cb = new ImageIcon("./imagenes/sin_fondo/Caballo_blanco.png");
-		ImageIcon tb = new ImageIcon("./imagenes/sin_fondo/Torre_blanca.png");
-		ImageIcon ab = new ImageIcon("./imagenes/sin_fondo/Alfil_blanco.png");
-		ImageIcon qb = new ImageIcon("./imagenes/sin_fondo/Reina_blanca.png");
-		ImageIcon kb = new ImageIcon("./imagenes/sin_fondo/Rey_blanco.png");
-
-		for (int i = 0; i < 8; i++)
-		{
-			for (int j = 0; j < 8; j++)
-			{
-				casillas[i][j].setIcon(null);
-			}
-		}
-
-		for (int i = 0; i < 8; i++)
-		{
-			for (int j = 0; j < 8; j++)
-			{
-				if (tablero[i][j].equals("Pn1")) {
-					casillas[i][j].setIcon(pn);
-				}
-				if (tablero[i][j].equals("Pn2")) {
-					casillas[i][j].setIcon(pn);
-				}
-				if (tablero[i][j].equals("Pn3")) {
-					casillas[i][j].setIcon(pn);
-				}
-				if (tablero[i][j].equals("Pn4")) {
-					casillas[i][j].setIcon(pn);
-				}
-				if (tablero[i][j].equals("Pn5")) {
-					casillas[i][j].setIcon(pn);
-				}
-				if (tablero[i][j].equals("Pn6")) {
-					casillas[i][j].setIcon(pn);
-				}
-				if (tablero[i][j].equals("Pn7")) {
-					casillas[i][j].setIcon(pn);
-				}
-				if (tablero[i][j].equals("Pn8")) {
-					casillas[i][j].setIcon(pn);
-				}
-
-				if (tablero[i][j].equals("Pb1")) {
-					casillas[i][j].setIcon(pb);
-				}
-				if (tablero[i][j].equals("Pb2")) {
-					casillas[i][j].setIcon(pb);
-				}
-				if (tablero[i][j].equals("Pb3")) {
-					casillas[i][j].setIcon(pb);
-				}
-				if (tablero[i][j].equals("Pb4")) {
-					casillas[i][j].setIcon(pb);
-				}
-				if (tablero[i][j].equals("Pb5")) {
-					casillas[i][j].setIcon(pb);
-				}
-				if (tablero[i][j].equals("Pb6")) {
-					casillas[i][j].setIcon(pb);
-				}
-				if (tablero[i][j].equals("Pb7")) {
-					casillas[i][j].setIcon(pb);
-				}
-				if (tablero[i][j].equals("Pb8")) {
-					casillas[i][j].setIcon(pb);
-				}
-				if (tablero[i][j].equals("Tn1")) {
-					casillas[i][j].setIcon(tn);
-				}
-				if (tablero[i][j].equals("Cn1")) {
-					casillas[i][j].setIcon(cn);
-				}
-				if (tablero[i][j].equals("An1")) {
-					casillas[i][j].setIcon(an);
-				}
-				if (tablero[i][j].equals("Qn1")) {
-					casillas[i][j].setIcon(qn);
-				}
-				if (tablero[i][j].equals("Kn1")) {
-					casillas[i][j].setIcon(kn);
-				}
-				if (tablero[i][j].equals("An2")) {
-					casillas[i][j].setIcon(an);
-				}
-				if (tablero[i][j].equals("Cn2")) {
-					casillas[i][j].setIcon(cn);
-				}
-				if (tablero[i][j].equals("Tn2")) {
-					casillas[i][j].setIcon(tn);
-				}
-				if (tablero[i][j].equals("Tb1")) {
-					casillas[i][j].setIcon(tb);
-				}
-				if (tablero[i][j].equals("Cb1")) {
-					casillas[i][j].setIcon(cb);
-				}
-				if (tablero[i][j].equals("Ab1")) {
-					casillas[i][j].setIcon(ab);
-				}
-				if (tablero[i][j].equals("Qb1")) {
-					casillas[i][j].setIcon(qb);
-				}
-				if (tablero[i][j].equals("Kb1")) {
-					casillas[i][j].setIcon(kb);
-				}
-				if (tablero[i][j].equals("Ab2")) {
-					casillas[i][j].setIcon(ab);
-				}
-				if (tablero[i][j].equals("Cb2")) {
-					casillas[i][j].setIcon(cb);
-				}
-				if (tablero[i][j].equals("Tb2")) {
-					casillas[i][j].setIcon(tb);
-				}
-			}
-		}
-	}*/
 
 	public boolean esMovimValido(int i, int j, int linea, int columna)
 	{
@@ -377,52 +202,43 @@ public class Modelo
 			ex.printStackTrace();
 		}
 	} 
-	
-	public Integer[][] movimientoCaballo(Caballo caballo) {
-		Integer[][] i = new Integer[8][2];
 
+	public Integer[][] movimientoCaballo(Pieza caballo) {
+		Integer[][] i = new Integer[8][2];
 		i[0][0] = caballo.getX()+1;
 		i[0][1] = caballo.getY()+2;
-
 		i[1][0] = caballo.getX()-1;
 		i[1][1] = caballo.getY()+2;
-
 		i[2][0] = caballo.getX()+1;
 		i[2][1] = caballo.getY()-2;
-
 		i[3][0] = caballo.getX()-1;
 		i[3][1] = caballo.getY()-2;
-
 		i[4][0] = caballo.getX()+2;
 		i[4][1] = caballo.getY()+1;
-
 		i[5][0] = caballo.getX()-2;
 		i[5][1] = caballo.getY()+1;
-
 		i[6][0] = caballo.getX()+2;
 		i[6][1] = caballo.getY()-1;
-		
 		i[7][0] = caballo.getX()-2;
 		i[7][1] = caballo.getY()-1;
-		
 		return i;
 	}
-	
-	
+
+
 
 	public String iconoPieza(JButton casilla) {
-		Torre torreN = new Torre(ColorPieza.NEGRO, 0, 0);
-		Caballo caballoN = new Caballo(ColorPieza.NEGRO, 1, 0);
-		Alfil alfilN = new Alfil(ColorPieza.NEGRO, 2, 0);
-		Reina reinaN = new Reina(ColorPieza.NEGRO, 3, 0);
-		Rey reyN = new Rey(ColorPieza.NEGRO, 4, 0);
-		Peon peonN = new Peon(ColorPieza.NEGRO, 0, 1);
-		Torre torreB = new Torre(ColorPieza.BLANCO, 0, 7);
-		Caballo caballoB = new Caballo(ColorPieza.BLANCO, 1, 7);
-		Alfil alfilB = new Alfil(ColorPieza.BLANCO, 2, 7);
-		Reina reinaB = new Reina(ColorPieza.BLANCO, 3, 7);
-		Rey reyB = new Rey(ColorPieza.BLANCO, 4, 7);
-		Peon peonB = new Peon(ColorPieza.BLANCO, 0, 6);
+		Pieza torreN = new Pieza(ColorPieza.NEGRO, TipoPieza.TORRE, 0, 0);
+		Pieza caballoN = new Pieza(ColorPieza.NEGRO, TipoPieza.CABALLO, 1, 0);
+		Pieza alfilN = new Pieza(ColorPieza.NEGRO, TipoPieza.ALFIL, 2, 0);
+		Pieza reinaN = new Pieza(ColorPieza.NEGRO, TipoPieza.REINA, 3, 0);
+		Pieza reyN = new Pieza(ColorPieza.NEGRO, TipoPieza.REY, 4, 0);
+		Pieza peonN = new Pieza(ColorPieza.NEGRO, TipoPieza.PEON, 0, 1);
+		Pieza torreB = new Pieza(ColorPieza.BLANCO, TipoPieza.TORRE, 0, 7);
+		Pieza caballoB = new Pieza(ColorPieza.BLANCO, TipoPieza.CABALLO, 1, 7);
+		Pieza alfilB = new Pieza(ColorPieza.BLANCO, TipoPieza.ALFIL, 2, 7);
+		Pieza reinaB = new Pieza(ColorPieza.BLANCO, TipoPieza.REINA, 3, 7);
+		Pieza reyB = new Pieza(ColorPieza.BLANCO, TipoPieza.REY, 4, 7);
+		Pieza peonB = new Pieza(ColorPieza.BLANCO, TipoPieza.PEON, 0, 6);
 		String res = "Vacío";
 		if(casilla.getIcon()==null) {
 			res = "Vacío";
@@ -448,10 +264,10 @@ public class Modelo
 			res = "Rey";
 		}
 		else if(casilla.getIcon().toString() == peonN.getIcono().toString()) {
-			res = "Peon Negro";
+			res = "Peon";
 		}
 		else if(casilla.getIcon().toString() == peonB.getIcono().toString()) {
-			res = "Peon Blanco";
+			res = "Peon";
 		}
 		return res;
 	}
@@ -512,7 +328,7 @@ public class Modelo
 
 			fh = new FileHandler("log.txt", true);
 			logger.addHandler(fh);
-			
+
 			SimpleFormatter formatter = new SimpleFormatter();
 			fh.setFormatter(formatter);
 
@@ -536,7 +352,7 @@ public class Modelo
 			} 
 
 			logger.info("[" + mensaje + "] \n");
-			
+
 			fh.close();
 		} catch (SecurityException e) {
 			System.out.println("error 1");
@@ -554,11 +370,9 @@ public class Modelo
 		}
 		else {
 			if(casilla.getIcon().toString().contains("negr")) {
-				System.out.println(casilla.getIcon().toString());
 				res = ColorPieza.NEGRO;
 			}
 			else if(casilla.getIcon().toString().contains("blanc")) {
-				System.out.println(casilla.getIcon().toString());
 				res = ColorPieza.BLANCO;
 			}
 		}
--- src/modelo/Pieza.java
+++ src/modelo/Pieza.java
@@ -1,23 +1,76 @@
 package modelo;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
 public class Pieza {
 
 	public ColorPieza colorPieza;
 	public TipoPieza tipoPieza;
-	public Integer posX, posY;
+	public Integer x, y;
+	public Icon icono;
 
 	public Pieza() {
 		this.colorPieza = ColorPieza.BLANCO;
 		this.tipoPieza = TipoPieza.PEON;
-		this.posX = null;
-		this.posY = null;
+		this.x = 0;
+		this.y = 0;
+		this.icono = null;
 	}
 	
-	public Pieza(ColorPieza colorPieza, TipoPieza tipoPieza, Integer posX, Integer posY) {
+	public Pieza(ColorPieza colorPieza, TipoPieza tipoPieza, Integer y, Integer x) {
 		this.colorPieza = colorPieza;
 		this.tipoPieza = tipoPieza;
-		this.posX = posX;
-		this.posY = posY;
+		this.x = x;
+		this.y = y;
+		if(tipoPieza == TipoPieza.ALFIL) {
+			if(colorPieza == ColorPieza.BLANCO) {
+				this.icono = new ImageIcon("imagenes/sin_fondo/Alfil_blanco.png");
+			}
+			else if(colorPieza == ColorPieza.NEGRO) {
+				this.icono = new ImageIcon("imagenes/sin_fondo/Alfil_negro.png");
+			}
+		}
+		else if(tipoPieza == TipoPieza.CABALLO) {
+			if(colorPieza == ColorPieza.BLANCO) {
+				this.icono = new ImageIcon("imagenes/sin_fondo/Caballo_blanco.png");
+			}
+			else if(colorPieza == ColorPieza.NEGRO) {
+				this.icono = new ImageIcon("imagenes/sin_fondo/Caballo_negro.png");
+			}
+		}
+		else if(tipoPieza == TipoPieza.PEON) {
+			if(colorPieza == ColorPieza.BLANCO) {
+				this.icono = new ImageIcon("imagenes/sin_fondo/Peon_blanco.png");
+			}
+			else if(colorPieza == ColorPieza.NEGRO) {
+				this.icono = new ImageIcon("imagenes/sin_fondo/Peon_negro.png");
+			}
+		}
+		else if(tipoPieza == TipoPieza.TORRE) {
+			if(colorPieza == ColorPieza.BLANCO) {
+				this.icono = new ImageIcon("imagenes/sin_fondo/Torre_blanca.png");
+			}
+			else if(colorPieza == ColorPieza.NEGRO) {
+				this.icono = new ImageIcon("imagenes/sin_fondo/Torre_negra.png");
+			}
+		}
+		else if(tipoPieza == TipoPieza.REINA) {
+			if(colorPieza == ColorPieza.BLANCO) {
+				this.icono = new ImageIcon("imagenes/sin_fondo/Reina_blanca.png");
+			}
+			else if(colorPieza == ColorPieza.NEGRO) {
+				this.icono = new ImageIcon("imagenes/sin_fondo/Reina_negra.png");
+			}
+		}
+		else if(tipoPieza == TipoPieza.REY) {
+			if(colorPieza == ColorPieza.BLANCO) {
+				this.icono = new ImageIcon("imagenes/sin_fondo/Rey_blanco.png");
+			}
+			else if(colorPieza == ColorPieza.NEGRO) {
+				this.icono = new ImageIcon("imagenes/sin_fondo/Rey_negro.png");
+			}
+		}
 	}
 
 	public ColorPieza getColorPieza() {
@@ -36,23 +89,31 @@ public class Pieza {
 		this.tipoPieza = tipoPieza;
 	}
 
-	public Integer getPosX() {
-		return posX;
+	public Integer getX() {
+		return x;
 	}
 
-	public void setPosX(Integer posX) {
-		this.posX = posX;
+	public void setX(Integer x) {
+		this.x = x;
 	}
 
-	public Integer getPosY() {
-		return posY;
+	public Integer getY() {
+		return y;
 	}
 
-	public void setPosY(Integer posY) {
-		this.posY = posY;
+	public void setY(Integer y) {
+		this.y = y;
 	}
 	
+	public Icon getIcono() {
+		return icono;
+	}
+
+	public void setIcono(Icon icono) {
+		this.icono = icono;
+	}
+
 	public String toString() {
-		return "Es un/a " + getTipoPieza() + " de color " + getColorPieza() + ", comienza en la posicion " + getPosX() + ", " + getPosY();
+		return "Es un/a " + getTipoPieza() + " de color " + getColorPieza() + ", comienza en la posicion " + getX() + ", " + getY();
 	}
 }
